9e8cc92e162022ce20224119d2291baa99d07ce0
denisdrabinka <denisdrabinka@mail.ru>
2019-11-13 15:06:04 +0300

    den 4

--- DoorStorage/DoorStorage/MainWindowViewModel.cs
+++ DoorStorage/DoorStorage/MainWindowViewModel.cs
@@ -523,7 +523,9 @@ namespace DoorStorage
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
 
-
+        ///deefege
+        ///ehehehehe
+        ///eehehhe
     }
 }
 
