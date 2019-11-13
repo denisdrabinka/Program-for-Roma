0025c2d89ca0ebb52b9984eca062f50d7760c96e
denisdrabinka <denisdrabinka@mail.ru>
2019-11-13 14:27:08 +0300

    comm from den2

--- DoorStorage/DoorStorage/MainWindowViewModel.cs
+++ DoorStorage/DoorStorage/MainWindowViewModel.cs
@@ -523,7 +523,9 @@ namespace DoorStorage
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
 
-        /// denis
+        /// denis hddjjdjhdhdjhdjhdjjhd
+        /// //dfidjdjdk
+        /// djdkdkjd
     }
 }
 
